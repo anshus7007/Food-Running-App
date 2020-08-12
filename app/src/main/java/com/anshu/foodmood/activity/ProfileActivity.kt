f8b5182d1f65bf3f85dbedd49921c55c42f6bdb6
anshus7007 <60043003+anshus7007@users.noreply.github.com>
2020-08-12 13:55:18 +0530

    initial commit

---
+++ app/src/main/java/com/anshu/foodmood/activity/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.anshu.foodmood.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anshu.foodmood.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
